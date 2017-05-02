cf24f48cc365e5b7c94139aeec1d5c827183569a
Ram Pasala <ram.pasala7@gmail.com>
2017-05-02 13:23:03 -0700

    chore(protractor-typescript-cucumber): add cucumber 2.0 support to protractor-typescript-cucumber) (#37)

--- protractor-typescript-cucumber/stepdefinitions/calcSteps.ts
+++ protractor-typescript-cucumber/stepdefinitions/calcSteps.ts
@@ -1,26 +1,28 @@
 import { browser } from 'protractor';
 import { CalculatorPageObject } from '../pages/calcPage';
+import { defineSupportCode } from 'cucumber';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 /*
 StepDefinition files act as the glue code between config and feature files
 They drive the feature files from the background
 **/
-export = function() {
+defineSupportCode(({Given, When, Then}) => {
   let calc: CalculatorPageObject = new CalculatorPageObject();
 
-  this.Given(/^I am on ng1 calculator page$/, () => {
+  Given(/^I am on ng1 calculator page$/, () => {
     return expect(browser.getTitle()).to.eventually.equal('Super Calculator');
   });
 
-  this.When(/^I calculate "(.*?)" "(.*?)" "(.*?)"$/, (num1: string, optor: string, num2: string) => {
+  When(/^I calculate "(.*?)" "(.*?)" "(.*?)"$/, (num1: string, optor: string, num2: string) => {
     calc.first_operand.sendKeys(num1);
     calc.operator(optor).click();
     calc.second_operand.sendKeys(num2);
     return calc.go_button.click();
   });
 
-  this.Then(/^the result "(.*?)" should be displayed$/, (result: string) => {
+  Then(/^the result "(.*?)" should be displayed$/, (result: string) => {
     return expect(calc.result.getText()).to.eventually.equal(result);
   });
-};
+})
+
